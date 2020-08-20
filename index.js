989b11d41c0ad31a1ff374538178708a79358006
Loreen-netizen <loreenmuusha@gmail.com>
2020-08-20 13:45:09 +0200

    body parser configured, set values now available

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
-const body = require("body-parser");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-let PORT = process.env.PORT || 3508 ;
+let PORT = process.env.PORT || 3508;
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log("App starting on port", PORT)
 });
 
 app.use(express.static("public"));
 
-app.get(("/"),function(req,res){
-res.render("index");
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get(("/"), function (req, res) {
+    res.render("index");
 });
 
-app.post(("/settings"),function(req,res){
+app.post(("/settings"), function (req, res) {
+    console.log(req.body)
+    res.redirect("/")
+});
 
-    });
+app.post(("/action"), function (req, res) {
 
-app.post(("/action"),function(req,res){
-    
-    });
+});
 
- app.get(("/actions"),function(req,res){
-    
-    });
+app.get(("/actions"), function (req, res) {
 
-app.get(("/actions/:type"),function(req,res){
-    
-    });
+});
+
+app.get(("/actions/:type"), function (req, res) {
+
+});
